a96e92f653cf51d3ee0a6c103bb39ebf593fe4c5
weak_ptr <weak_ptr@outlook.com>
2022-03-16 14:15:15 +0800

    feat: put命令添加分片大小参数，优化进度条效果

--- cmd/put.go
+++ cmd/put.go
@@ -57,8 +57,8 @@ func init() {
 
 	// Cobra supports Persistent Flags which will work for this command
 	// and all subcommands, e.g.:
-	putCmd.Flags().StringP("dest", "d", "", "上传位置，最终key会拼接成 <dest>/filename 的形式")
-
+	putCmd.Flags().StringP("dest", "d", "", "上传位置, 最终key会拼接成 <dest>/filename 的形式")
+	putCmd.Flags().Int64P("partsize", "p", 1024*1024, "分片大小, 分片会并发上传")
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
 	// putCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
--- internal/utils/upload.go
+++ internal/utils/upload.go
@@ -11,6 +11,7 @@ import (
 	"github.com/pkg/errors"
 	"github.com/qiniu/go-sdk/v7/auth"
 	"github.com/qiniu/go-sdk/v7/storage"
+	"github.com/spf13/viper"
 )
 
 func Upload(bucket, key, path string, credential *auth.Credentials) error {
@@ -35,21 +36,25 @@ func Upload(bucket, key, path string, credential *auth.Credentials) error {
 	}
 
 	mutex := sync.Mutex{}
+	partsize := viper.GetInt64("partsize")
 	total := info.Size()
-	progress := pb.New64(total)
-	progress.SetRefreshRate(time.Second)
-	progress.Start()
-	err = uploader.Put(context.TODO(), &ret, token, key, file, total, &storage.RputV2Extra{
-		PartSize: 1048576,
+	progress := pb.
+		New64(total).
+		SetRefreshRate(time.Second).
+		SetWidth(79).
+		Set(pb.Bytes, true).
+		Start()
+	err = uploader.Put(context.Background(), &ret, token, key, file, total, &storage.RputV2Extra{
+		PartSize: partsize,
 		Notify: func(partNumber int64, ret *storage.UploadPartsRet) {
 			mutex.Lock()
 			defer mutex.Unlock()
-			if total-1048576 < 0 {
+			if total-partsize < 0 {
 				progress.Add64(total)
 			} else {
-				progress.Add64(1048576)
+				progress.Add64(partsize)
 			}
-			total -= 1048576
+			total -= partsize
 		},
 		NotifyErr: func(partNumber int64, err error) {
 			mutex.Lock()
