68f6c470b81d87586b47b662812ade751b971aff
weak_ptr <weak_ptr@outlook.com>
2022-03-16 11:00:10 +0800

    feat: 支持上传进度条

--- internal/utils/upload.go
+++ internal/utils/upload.go
@@ -4,8 +4,10 @@ import (
 	"context"
 	"fmt"
 	"os"
+	"sync"
 	"time"
 
+	"github.com/cheggaaa/pb/v3"
 	"github.com/pkg/errors"
 	"github.com/qiniu/go-sdk/v7/auth"
 	"github.com/qiniu/go-sdk/v7/storage"
@@ -18,7 +20,7 @@ func Upload(bucket, key, path string, credential *auth.Credentials) error {
 		Expires: uint64(time.Now().Unix()) + uint64(time.Hour.Seconds()),
 	}
 	token := policy.UploadToken(credential)
-	uploader := storage.NewFormUploader(&storage.Config{})
+	uploader := storage.NewResumeUploaderV2(&storage.Config{})
 
 	file, err := os.OpenFile(path, os.O_RDONLY, 0644)
 	if err != nil {
@@ -32,7 +34,30 @@ func Upload(bucket, key, path string, credential *auth.Credentials) error {
 		return errors.Wrap(err, "check upload file stat fail")
 	}
 
-	err = uploader.Put(context.TODO(), &ret, token, key, file, info.Size(), &storage.PutExtra{})
+	mutex := sync.Mutex{}
+	total := info.Size()
+	progress := pb.New64(total)
+	progress.SetRefreshRate(time.Second)
+	progress.Start()
+	err = uploader.Put(context.TODO(), &ret, token, key, file, total, &storage.RputV2Extra{
+		PartSize: 1048576,
+		Notify: func(partNumber int64, ret *storage.UploadPartsRet) {
+			mutex.Lock()
+			defer mutex.Unlock()
+			if total-1048576 < 0 {
+				progress.Add64(total)
+			} else {
+				progress.Add64(1048576)
+			}
+			total -= 1048576
+		},
+		NotifyErr: func(partNumber int64, err error) {
+			mutex.Lock()
+			defer mutex.Unlock()
+			progress.SetErr(err)
+		},
+	})
+	progress.Finish()
 	if err != nil {
 		return errors.Wrap(err, "putfile fail")
 	}
